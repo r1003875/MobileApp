5807e664e7cee1e01e931aaf9d3b28ad8870dc12
r1003875 <147533527+r1003875@users.noreply.github.com>
2025-02-19 14:58:41 +0100

    feat: home

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, ScrollView, View, Button, Alert } from 'react-native';
-import ProductCard from './components/ProductCard';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import ProductDetail from './screens/ProductDetail';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>Gouden Carolus</Text>
-      <StatusBar style="auto" />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={ProductDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1f1f1f',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50,
-    marginTop: 20,
-    gap: 5,
-  },
-  h1: {
-    color: '#ac9c51',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ProductCard from '../components/ProductCard';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>Gouden Carolus</Text>
+      <StatusBar style="auto" />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f1f1f',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+    marginTop: 20,
+    gap: 5,
+  },
+  h1: {
+    color: '#ac9c51',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
