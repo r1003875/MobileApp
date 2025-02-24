6752efddff9378173b124825bdbe33f03535774a
r1003875 <147533527+r1003875@users.noreply.github.com>
2025-02-24 14:40:35 +0100

    feat: user can navigate back from details page to home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import ProductCard from '../components/ProductCard';
 
 
@@ -11,9 +11,6 @@ export default function HomeScreen({navigation}) {
       <StatusBar style="auto" />
       <ProductCard />
       <ProductCard />
-        <TouchableOpacity onPress={() => navigation.navigate('Details')}>
-            <Text style={{color: '#ac9c51', fontSize: 18, marginTop: 20}}>Bekijk product</Text>
-        </TouchableOpacity>
     </View>
   );
 }
@@ -37,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#1f1f1f',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 50,
     marginTop: 20,
     gap: 5,
--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import ProductCard from '../components/ProductCard';
 
 export default function Details({navigation}) {
@@ -8,8 +8,12 @@ export default function Details({navigation}) {
     <View style={styles.container}>
       <Text style={styles.h1}>Details</Text>
       <StatusBar style="auto" />
-      <ProductCard />
-      <ProductCard />
+        <Image source={{uri: 'https://www.babylondrinks.be/wp-content/uploads/2021/10/Gouden-Carolus-Classic-24x33cl.jpg'}} style={styles.image} />
+        <Text style={styles.description}>Gouden Carolus Classic - 33cl</Text>
+        <Text style={styles.price}>€1.90</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <Text style={{color: '#ac9c51', fontSize: 18, marginTop: 20}}>Terug</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -39,5 +43,18 @@ const styles = StyleSheet.create({
     color: '#ac9c51',
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  image:{
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+  },
+  description: {
+      color: '#fffef9',
+      fontSize: 18,
+  },
+  price: {
+      color: '#ac9c51',
+      fontSize: 16,
   },
 });
